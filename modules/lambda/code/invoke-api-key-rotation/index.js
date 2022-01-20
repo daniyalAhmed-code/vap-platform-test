8243f48b929b2257098405e6f836d876c878e8a0
daniyalAhmed-code <70039650+daniyalAhmed-code@users.noreply.github.com>
2022-01-21 03:16:24 +0500

    update: Invoke Api key rotation

--- modules/lambda/code/invoke-api-key-rotation/index.js
+++ modules/lambda/code/invoke-api-key-rotation/index.js
@@ -2,11 +2,11 @@ const customersController = require('dev-portal-common/customers-controller')
 
 
 exports.handler = async (event) => {
-    console.log(event)
     let api_date
     let identityId = event.Id
     let userId     = event.UserPoolId
-    let apiKeyVal = event.api_data.id
+    let stage = event.Name
+    let api_id = event.api_data.value
     let current_date = new Date()
     
     if ('lastUpdatedDate' in event.api_data) {
@@ -17,22 +17,20 @@ exports.handler = async (event) => {
     
     let ApiDate = new Date(api_date);
     ApiDate.setDate(ApiDate.getDate() + event.ApiKeyDuration);
-
-    if (ApiDate < current_date && event.KeyRotation) {
+    if (ApiDate > current_date && event.KeyRotationEnabled) {
         let usagePlanId = await new Promise((resolve, reject) => {customersController.getUsagePlansForCustomer(userId, reject, resolve) });
         if (usagePlanId.items.hasOwnProperty("id")) {
-                
             usagePlanId = usagePlanId.items[0].id
-            await new Promise((resolve, reject) => {customersController.unsubscribe(userId, reject, resolve) });
-            await customersController.deletePreviousApiKey(event.value)
-            await customersController.renewApiKey(identityId, userId, true);
-            await new Promise((resolve, reject) => {customersController.subscribe(userId, reject, resolve) });
-            
-            
+            await new Promise((resolve, reject) => {customersController.unsubscribe(identityId, usagePlanId, reject, resolve)})
+            await customersController.deletePreviousApiKey(api_id)
+            let newApiKey = await customersController.renewApiKey(identityId,userId, stage, true);
+            await updateUser(identityId,newApiKey,stage)
+            await new Promise((resolve, reject) => {customersController.subscribe(identityId, usagePlanId, reject, resolve)})
         }
         else {
-            await customersController.deletePreviousApiKey(event.value)
-            await customersController.renewApiKey(identityId, userId, true);
+            let deleteapikey = await customersController.deletePreviousApiKey(api_id)
+            let newApiKey =await customersController.renewApiKey(identityId,userId, stage, true);
+            await updateUser(identityId,newApiKey,stage)
         }
     }    
     const response = {
@@ -41,3 +39,15 @@ exports.handler = async (event) => {
     };
     return response;
 };
+
+async function updateUser(identityId,newApiKey,stage) {
+    let userdetails = await customersController.getAccountDetails(identityId)
+        
+    for(let user_stage of userdetails.ApiKeyId.stage)
+    {
+        if(user_stage.Name == stage)
+            user_stage['id'] = newApiKey.id
+    }
+    let apis = userdetails.ApiKeyId.stage
+    let updateuser = await customersController.updateCustomerApiKeyId(identityId,apis)
+}
