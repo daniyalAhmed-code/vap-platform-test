10f481f098484a54b2e241b35fc995cedb30461d
daniyalAhmed-code <70039650+daniyalAhmed-code@users.noreply.github.com>
2022-01-20 23:53:43 +0500

    Revert "update: changed according to new user structure"
    This reverts commit 2f3cb3a36457ccdabafa92bb90f06fcb860fe284.

--- modules/lambda/code/generate-apikey-for-existing-users/index.js
+++ modules/lambda/code/generate-apikey-for-existing-users/index.js
@@ -1,20 +1,30 @@
 'use strict'
 
 const customersController = require('dev-portal-common/customers-controller')
+const { promisify2 } = require('dev-portal-common/promisify2')
+const util = require('dev-portal-common/util')
 
 const rh   =  require('dev-portal-common/responsehandler')
+const common = require('dev-portal-common/common')
 
 exports.handler = async (req, res) => {
 
-  let listAllUsers = await customersController.listAllUsers()
-  for (let user of listAllUsers.Items){
-      if (!user.ApiKeyId.hasOwnProperty("stage")){
-        let userId = user.Id
-        let username = user.Username
-        await customersController.createExistingUsersApiKey(userId,username)
+    let tableName = `${process.env.CustomerTableName}`
+    let params = {
+        TableName : tableName,
       }
+    let listAllUsers = await customersController.listAllUsers()
+    for (let user of listAllUsers.Items){
+        if (typeof user.ApiKeyId == "string" && user.ApiKeyId !== "NO_API_KEY"){
+            let apiKey = user.ApiKeyId
+                await customersController.createExistingUsersApiKey({
+                  userId: user.Username,
+                  identityId: user.Id,
+                  apiKeyId:apiKey
+                })
+        }
 
-  }
-  return rh.callbackRespondWithSimpleMessage(200,"Success")
+    }
+    return rh.callbackRespondWithSimpleMessage(200,"Success")
 
 }
